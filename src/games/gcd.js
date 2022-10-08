35899923f67e4b80c8e33be876b9789cbc629afc
Artyom Kropp <artyomkropp@gmail.com>
2022-10-09 00:59:52 +0900

    add brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import numGenerator from '../utils.js';
+
+import gameGenerator from '../engine.js'
+
+const gameRules = 'Find the greatest common divisor of given numbers.';
+
+const gcd = (firstNumber, secondNumber) => {
+  if (!secondNumber) {
+    return firstNumber;
+  }
+  return gcd(secondNumber, firstNumber % secondNumber);
+}
+
+const gcdGame = () => {
+  const firstNumber = numGenerator(1, 100);
+  const secondNumber = numGenerator(1,100);
+  const question = `${firstNumber} ${secondNumber}`;
+  const correctAnswer = gcd(firstNumber, secondNumber);
+  return [String(correctAnswer), question];
+};
+
+export default gameGenerator(gcdGame, gameRules);
