ef9f896e7b2d5acdc7e0084224a79f26b3bc3e61
Artyom Kropp <artyomkropp@gmail.com>
2022-10-17 20:27:01 +0900

    update naming

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,21 +4,21 @@ import generateGame from '../engine.js';
 
 const gameRules = 'What number is missing in the progression?';
 
-const generateArr = (length, step) => {
-  const questionArr = [];
+const getProgression = (length, step) => {
+  const question = [];
   const firstNum = generateNum(5, 50);
 
   for (let i = 0; i < length; i += 1) {
     const num = firstNum + step * i;
-    questionArr.push(num);
+    question.push(num);
   }
-  return questionArr;
+  return question;
 };
 
 const generateProgressionGameRound = () => {
-  const arrLength = generateNum(5, 10);
+  const length = generateNum(5, 10);
   const step = generateNum(2, 20);
-  const question = generateArr(arrLength, step);
+  const question = getProgression(length, step);
   const randomIndex = generateNum(0, question.length - 1);
   const answer = question.splice(randomIndex, 1, '..');
   return [answer.join(), question.join(' ')];
